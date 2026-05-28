7e69472bcd50dbd1ab3bbb365eae021175ea7868
tararychkin <dnxcrew@mail.ru>
2026-05-28 20:01:02 +0300

    создал консоль меню

---
+++ java/MainApp.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            printMenu();
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+
+                case "1":
+                    System.out.println("Шифрование");
+                    break;
+
+                case "2":
+                    System.out.println("Расшифровка");
+                    break;
+
+                case "0":
+                    System.out.println("Выход");
+                    return;
+
+                default:
+                    System.out.println("Неверный пункт меню.");
+            }
+        }
+    }
+
+    private static void printMenu() {
+
+        System.out.println();
+        System.out.println("===== Caesar Cipher =====");
+        System.out.println("1. Шифрование текста");
+        System.out.println("2. Расшифровка текста");
+        System.out.println("0. Выход");
+        System.out.print("Выберите действие: ");
+    }
+}
